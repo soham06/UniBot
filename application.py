eedfb06b012c94398d3a55ef51ec5afe44c5890f
soham06 <65688385+soham06@users.noreply.github.com>
2021-02-21 06:58:39 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect, session
 import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
-from helper import *
 
 app = Flask(__name__)
 app.secret_key = "hello"
